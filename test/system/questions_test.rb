729a0039589bb9cc447e37a5c822a57049e68bc1
antoine chilewski <chilewski.a@gmail.com>
2023-05-22 12:51:23 +0200

    do news tests

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -13,4 +13,20 @@ class QuestionsTest < ApplicationSystemTestCase
 
       assert_text "I don't care, get dressed and go to work!"
     end
+
+    test "saying the right sentence yields great from the coach" do
+      visit ask_url
+      fill_in "question", with: "I am going to work"
+      click_on "Ask"
+
+      assert_text "Great!"
+    end
+
+    test "saying a question yields a grumpy response from the coach" do
+      visit ask_url
+      fill_in "question", with: "go shopping ?"
+      click_on "Ask"
+
+      assert_text "Silly question, get dressed and go to work!"
+    end
 end
